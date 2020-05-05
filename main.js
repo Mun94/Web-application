db2d7d603d0f5ff4016f0c6adc9ac283b0e306d6
seongwoo94 <seongwoo720@gmail.com>
2020-05-05 15:57:16 +0900

    리팩토링 : 코드 리팩토링(templateList, templateHTML)

--- main.js
+++ main.js
@@ -2,6 +2,32 @@ const http = require('http');
 const fs = require('fs');
 const url = require('url');
 
+templateList = filelist => {
+    let list = "<ul>";
+    for(let i=0; i<filelist.length; i++)
+    {
+        list = list + `<li><a href = "/?id=${filelist[i]}">${filelist[i]}</a></li>`
+    }
+    list = list + "</ul>";
+    return list;
+}
+
+templateHTML = (title, list, body) => {
+    return `<!doctype html>
+            <html>
+            <head>
+                <meta charset = "utf-8">
+                <title>${title} page</title>
+            <head>
+
+            <body>
+                <h1><a href = '/'>WEB</a></h1>
+                ${list}
+                ${body}
+            </body>
+            </html>`;
+}
+
 const app = http.createServer((request, response) => {
     const _url = request.url;
     const queryData = url.parse(_url, true).query;
@@ -15,57 +41,19 @@ const app = http.createServer((request, response) => {
         fs.readdir(`data`, (err, filelist) => {
             title = 'Welcome';
             const description = 'Hello nodejs';
+            const list = templateList(filelist);
 
-            let list = "<ul>";
-            for(let i=0; i<filelist.length; i++)
-            {
-                list = list + `<li><a href = "/?id=${filelist[i]}">${filelist[i]}</a></li>`
-            }
-            list = list + "</ul>";
-
-            const template = `<!doctype html>
-            <html>
-            <head>
-                <meta charset = "utf-8">
-                <title>${title} page</title>
-            <head>
-
-            <body>
-                <h1><a href = '/'>WEB</a></h1>
-                ${list}
-                <h2>${title}</h2>
-                ${description}
-            </body>
-            </html>`;
-        response.writeHead(200);
-        response.end(template);
+            const template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+            response.writeHead(200);
+            response.end(template);
     })
     ) :
     (
         fs.readdir('data', (err, filelist) => {
-            let list = "<ul>";
-            for(let i=0; i<filelist.length; i++)
-            {
-                list = list + `<li><a href = "/?id=${filelist[i]}">${filelist[i]}</a></li>`
-            }
-            list = list + "</ul>";
-
             fs.readFile(`data/${queryData.id}`, 'utf8', (err, description) => {
-            
-                const template = `<!doctype html>
-                <html>
-                <head>
-                    <meta charset = "utf-8">
-                    <title>${title} page</title>
-                <head>
-    
-                <body>
-                    <h1><a href = '/'>WEB</a></h1>
-                    ${list}
-                    <h2>${title}</h2>
-                    ${description}
-                </body>
-                </html>`;
+            const list = templateList(filelist);
+
+            const template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
             response.writeHead(200);
             response.end(template);
         })
