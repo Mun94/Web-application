b0848f0e20df576d45fdc294e75c94cf27cd5aa9
seongwoo94 <seongwoo720@gmail.com>
2020-05-07 18:36:13 +0900

    형식 : 코드 형식 변경(라우터 주소체제변경)
    /topic/~ 형식으로 변경했습니다. 그리고 app.get('/topic/:pageId', ~)을 끝으로 옮겼습니다.

--- main.js
+++ main.js
@@ -33,7 +33,68 @@ app.get('/', (request, response) => {
         response.send(html);
 });
 
-app.get('/page/:pageId', (request, response, next) => {
+app.get('/topic/create', (request, response) => {
+        list =template.List(request.list);
+        title = 'WEB - create';
+        html = template.HTML(title,list,``,`
+        <h2>create</h2>
+        <form action ="/topic/create_process" method = "post">
+        <p><input type = "text" name = "title" placeholder = "title"></p>
+        <p><textarea name = "description" placeholder = "description"></textarea></p>
+        <p><input type = "submit"></p></form>`);
+        
+        response.send(html);
+    })
+
+app.post(`/topic/create_process`, (request, response) => {
+        let post = request.body;
+        title = post.title;
+        description = post.description;
+        fs.writeFile(`data/${title}`, description, 'utf8', err => {
+            response.redirect(`/topic/page/${title}`);
+        })
+    })
+
+
+app.get(`/topic/update/:pageId`, (request, response) => {
+        const filteredId = path.parse(request.params.pageId).base;
+        fs.readFile(`data/${filteredId}`, 'utf8', (err, description) => {
+            list = template.List(request.list);
+            title = request.params.pageId;
+            html = template.HTML(title, list, `<h2>update</h2>
+            <form action = "/topic/update_process" method = "post">
+            <input type = "hidden" name = "id" value = "${title}">
+            <p><input type = "text" name = "title" value = "${title}"></p>
+            <p><textarea name = "description">${description}</textarea></p>
+            <p><input type = "submit"></p></form>
+            `,``);
+            
+            response.send(html);
+        })
+})
+app.post(`/topic/update_process`, (request, response) => {
+        let post = request.body;
+        let id = post.id;
+        title = post.title;
+        description = post.description;
+        fs.rename(`data/${id}`, `data/${title}`, err => {
+            fs.writeFile(`data/${title}`, description, 'utf8', err => {
+                response.redirect(`/topic/page/${title}`);
+            })
+        })
+    })
+
+
+app.post(`/topic/delete_process`, (request, response) => {
+        let post = request.body;
+        let id = post.id;
+
+        fs.unlink(`data/${id}`, err => {
+            response.redirect(`/`);
+        })
+    })
+
+    app.get('/topic/:pageId', (request, response, next) => {
         const filteredId = path.parse(request.params.pageId).base;
         fs.readFile(`data/${filteredId}`, 'utf8', (err2, description) => {
             if(err2){
@@ -50,9 +111,9 @@ app.get('/page/:pageId', (request, response, next) => {
                 'a':['href']
             }
         })
-        html = template.HTML(title, list,`<a href = "/create">create</a>
-         <a href = "/update/${sanitizedTitle}">update</a>
-         <form action = "/delete_process" method = "post" onsubmit="return confirm('do you want to delete this file?')">
+        html = template.HTML(title, list,`<a href = "/topic/create">create</a>
+         <a href = "/topic/update/${sanitizedTitle}">update</a>
+         <form action = "/topic/delete_process" method = "post" onsubmit="return confirm('do you want to delete this file?')">
          <p><input type = "hidden" name="id" value="${sanitizedTitle}"></p>
          <p><input type="submit" value="delete"></p></form>
          `, `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`);
@@ -60,67 +121,6 @@ app.get('/page/:pageId', (request, response, next) => {
          response.send(html);
         }})
 })
-
-app.get('/create', (request, response) => {
-        list =template.List(request.list);
-        title = 'WEB - create';
-        html = template.HTML(title,list,``,`
-        <h2>create</h2>
-        <form action ="/create_process" method = "post">
-        <p><input type = "text" name = "title" placeholder = "title"></p>
-        <p><textarea name = "description" placeholder = "description"></textarea></p>
-        <p><input type = "submit"></p></form>`);
-        
-        response.send(html);
-    })
-
-app.post(`/create_process`, (request, response) => {
-        let post = request.body;
-        title = post.title;
-        description = post.description;
-        fs.writeFile(`data/${title}`, description, 'utf8', err => {
-            response.redirect(`/page/${title}`);
-        })
-    })
-
-
-app.get(`/update/:pageId`, (request, response) => {
-        const filteredId = path.parse(request.params.pageId).base;
-        fs.readFile(`data/${filteredId}`, 'utf8', (err, description) => {
-            list = template.List(request.list);
-            title = request.params.pageId;
-            html = template.HTML(title, list, `<h2>update</h2>
-            <form action = "/update_process" method = "post">
-            <input type = "hidden" name = "id" value = "${title}">
-            <p><input type = "text" name = "title" value = "${title}"></p>
-            <p><textarea name = "description">${description}</textarea></p>
-            <p><input type = "submit"></p></form>
-            `,``);
-            
-            response.send(html);
-        })
-})
-app.post(`/update_process`, (request, response) => {
-        let post = request.body;
-        let id = post.id;
-        title = post.title;
-        description = post.description;
-        fs.rename(`data/${id}`, `data/${title}`, err => {
-            fs.writeFile(`data/${title}`, description, 'utf8', err => {
-                response.redirect(`/page/${title}`);
-            })
-        })
-    })
-
-
-app.post(`/delete_process`, (request, response) => {
-        let post = request.body;
-        let id = post.id;
-
-        fs.unlink(`data/${id}`, err => {
-            response.redirect(`/`);
-        })
-    })
 
 app.use((req,res,next) => {
     res.status(404).send('sorry');
