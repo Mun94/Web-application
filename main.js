9332a50168c16cc2e190158ca7cf70f091e389a4
seongwoo94 <seongwoo720@gmail.com>
2020-05-07 15:43:04 +0900

    기능 : 페이지 수정 기능 추가

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ app.get('/page/:pageId', (request, response) => {
             }
         })
         html = template.HTML(title, list,`<a href = "/create">create</a>
-         <a href = "/update?id=${sanitizedTitle}">update</a>
+         <a href = "/update/${sanitizedTitle}">update</a>
          <form action = "/delete_process" method = "post" onsubmit="return confirm('do you want to delete this file?')">
          <p><input type = "hidden" name="id" value="${sanitizedTitle}"></p>
          <p><input type="submit" value="delete"></p></form>
@@ -77,8 +77,43 @@ app.post(`/create_process`, (request, response) => {
         title = post.title;
         description = post.description;
         fs.writeFile(`data/${title}`, description, 'utf8', err => {
-            response.writeHead(302,{Location : `/page/${title}`});
-            response.end();
+            response.redirect(`/page/${title}`);
+        })
+    })
+})
+
+app.get(`/update/:pageId`, (request, response) => {
+    fs.readdir('data', (err, filelist) => {
+        const filteredId = path.parse(request.params.pageId).base;
+        fs.readFile(`data/${filteredId}`, 'utf8', (err, description) => {
+            list = template.List(filelist);
+            title = request.params.pageId;
+            html = template.HTML(title, list, `<h2>update</h2>
+            <form action = "/update_process" method = "post">
+            <input type = "hidden" name = "id" value = "${title}">
+            <p><input type = "text" name = "title" value = "${title}"></p>
+            <p><textarea name = "description">${description}</textarea></p>
+            <p><input type = "submit"></p></form>
+            `,``);
+            
+            response.send(html);
+        })
+    })
+})
+app.post(`/update_process`, (request, response) => {
+    let body="";
+    request.on('data', data => {
+        body = body + data;
+    }),
+    request.on('end', ()=>{
+        let post = qs.parse(body);
+        let id = post.id;
+        title = post.title;
+        description = post.description;
+        fs.rename(`data/${id}`, `data/${title}`, err => {
+            fs.writeFile(`data/${title}`, description, 'utf8', err => {
+                response.redirect(`/page/${title}`);
+            })
         })
     })
 })
