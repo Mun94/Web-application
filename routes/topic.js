bca0f8907e2916fab7786174e2e445d588c0535a
seongwoo94 <seongwoo720@gmail.com>
2020-05-12 20:09:48 +0900

    기능 : 접근제어:글 수정

--- routes/topic.js
+++ routes/topic.js
@@ -47,35 +47,70 @@ router.post(`/create_process`, (request, response) => {
 
 
 router.get(`/update/:pageId`, (request, response) => {
-    const filteredId = path.parse(request.params.pageId).base;
+    if (!check.IS(request,response)) {
+        response.redirect('/');
+        return false;
+      }
+      var topic = db.get('topics').find({id:request.params.pageId}).value();
+      request.flash('error', 'Not yours!');
+      if(topic.user_id !== request.user.id){
+        request.flash('error', 'Not yours!');
+        return response.redirect('/');
+      } 
+      var title = topic.title;
+      var description = topic.description;
+      var list = template.List(request.list);
+      var html = template.HTML(title,`${check.UI(request,response)}`, list, `<h2>update</h2>
+      <form action = "/topic/update_process" method = "post">
+      <input type = "hidden" name = "id" value = "${topic.id}">
+      <p><input type = "text" name = "title" value = "${title}"></p>
+      <p><textarea name = "description">${description}</textarea></p>
+      <p><input type = "submit"></p></form>
+      `,``);
+      response.send(html);
+    // const filteredId = path.parse(request.params.pageId).base;
+    // const topic = db.get('topics').find({id:request.params.pageId}).value();
 
-    !check.IS(request,response) ? 
-    response.send(`로그인이 필요합니다.<p><a href="/auth/login">login</a></p>`) : (
-    fs.readFile(`data/${filteredId}`, 'utf8', (err, description) => {
-        list = template.List(request.list);
-        title = request.params.pageId;
-        html = template.HTML(title,`${check.UI(request,response)}`, list, `<h2>update</h2>
-        <form action = "/topic/update_process" method = "post">
-        <input type = "hidden" name = "id" value = "${title}">
-        <p><input type = "text" name = "title" value = "${title}"></p>
-        <p><textarea name = "description">${description}</textarea></p>
-        <p><input type = "submit"></p></form>
-        `,``);
+    // !check.IS(request,response) ? 
+    // response.send(`로그인이 필요합니다.<p><a href="/auth/login">login</a></p>`) : (
         
-        response.send(html);
-    }))
+    // fs.readFile(`data/${filteredId}`, 'utf8', (err, description) => {
+        // list = template.List(request.list),
+        // title = topic.title,
+        // description = topic.description,
+
+        // html = template.HTML(title,`${check.UI(request,response)}`, list, `<h2>update</h2>
+        // <form action = "/topic/update_process" method = "post">
+        // <input type = "hidden" name = "id" value = "${topic.id}">
+        // <p><input type = "text" name = "title" value = "${title}"></p>
+        // <p><textarea name = "description">${description}</textarea></p>
+        // <p><input type = "submit"></p></form>
+        // `,``),
+        
+        // response.send(html)
+    //})
+    //)
 })
 router.post(`/update_process`, (request, response) => {
     let post = request.body;
     let id = post.id;
     title = post.title;
     description = post.description;
-    fs.rename(`data/${id}`, `data/${title}`, err => {
-        fs.writeFile(`data/${title}`, description, 'utf8', err => {
-            response.redirect(`/topic/${title}`);
-        })
-    })
-})
+    const topic = db.get('topics').find({id:id}).value();
+    if(topic.user_id !== request.user.id) {
+        request.flash('error', 'not yours!');
+        return response.redirect('/');
+     }
+    db.get('topics').find({id:id}).assign({
+        title:title, description:description
+    }).write();
+    response.redirect(`/topic/${topic.id}`);
+    // fs.rename(`data/${id}`, `data/${title}`, err => {
+    //     fs.writeFile(`data/${title}`, description, 'utf8', err => {
+    //         response.redirect(`/topic/${title}`);
+    //     })
+    // })
+});
 
 
 router.post(`/delete_process`, (request, response) => {
@@ -107,14 +142,14 @@ router.get('/:pageId', (request, response, next) => {
         list = template.List(request.list);
 
         const sanitizedTitle = sanitizeHtml(topic.title);
-        const sanitizedDescription = sanitizeHtml(description, {
+        const sanitizedDescription = sanitizeHtml(topic.description, {
             allowedTags:['h1', 'p', 'a'],
             allowedAttributes : {
                 'a':['href']
             }
         })
         html = template.HTML(title,`${check.UI(request,response)}`, list,`<a href = "/topic/create">create</a>
-        <a href = "/topic/update/${sanitizedTitle}">update</a>
+        <a href = "/topic/update/${topic.id}">update</a>
         <form action = "/topic/delete_process" method = "post" onsubmit="return confirm('do you want to delete this file?')">
         <p><input type = "hidden" name="id" value="${sanitizedTitle}"></p>
         <p><input type="submit" value="delete"></p></form>
